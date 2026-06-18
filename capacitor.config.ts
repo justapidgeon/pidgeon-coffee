8954b06227670b26d3bb58da116d88b30a40f4eb
Josiah Flores <josiahflores343@gmail.com>
2026-06-18 17:36:45 -0400

    Updated

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.pidgeoncoffee.app',
+  appName: 'pidgeon coffee',
+  webDir: 'out'
+};
+
+export default config;
